4b77dc4e0f4a68ad9aa0bd76642e342b797bf741
Verymew <111842541+verymew@users.noreply.github.com>
2024-05-29 08:35:27 -0300

    .

--- src/main/java/com/ju/api/services/TokenService.java
+++ src/main/java/com/ju/api/services/TokenService.java
@@ -6,7 +6,6 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.ju.api.config.LoggerC;
 import com.ju.api.models.UserModel;
 import com.ju.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
